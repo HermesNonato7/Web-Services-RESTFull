5bd946bf46db4b5467be8ebcee900031a4f6c4bf
HermesNonato7 <hermes.nonato7@hotmail.com>
2024-01-08 12:40:44 -0300

    add melhoria no codigo da classe Application

--- spring/src/main/java/com/minhaempresa/spring/Application.java
+++ spring/src/main/java/com/minhaempresa/spring/Application.java
@@ -7,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-
-        //System.out.println("Olá mundo!");
         SpringApplication.run(Application.class, args);
     }
-
 }
