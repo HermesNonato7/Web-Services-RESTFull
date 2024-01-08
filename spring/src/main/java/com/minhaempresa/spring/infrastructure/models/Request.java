6954edc1cacc109637b5daa4ef415a0c7854b28e
HermesNonato7 <hermes.nonato7@hotmail.com>
2024-01-08 13:31:20 -0300

    add annptation classe Request

--- spring/src/main/java/com/minhaempresa/spring/infrastructure/models/Request.java
+++ spring/src/main/java/com/minhaempresa/spring/infrastructure/models/Request.java
@@ -1,8 +1,8 @@
 package com.minhaempresa.spring.infrastructure.models;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
 
-import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Objects;
