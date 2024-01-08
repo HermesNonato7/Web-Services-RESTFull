2582ec4c451cbe0b3ee38de75345f915e37ec315
HermesNonato7 <hermes.nonato7@hotmail.com>
2024-01-08 10:51:36 -0300

    add

---
+++ spring/src/main/java/com/minhaempresa/spring/Application.java
@@ -0,0 +1,15 @@
+package com.minhaempresa.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        //System.out.println("Olá mundo!");
+        SpringApplication.run(Application.class, args);
+    }
+
+}
